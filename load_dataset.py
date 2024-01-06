b18a68ddce97836c0b53e63e4fe26f4f8b169cef
Katinza <149607371+Katinza@users.noreply.github.com>
2024-01-06 18:27:43 +0200

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,43 +1,43 @@
-# import the necessary packages
+# Βιβλιοθήκες
 from tensorflow.keras.datasets import mnist
 import numpy as np
 
 def load_az_dataset(datasetPath):
-	# initialize the list of data and labels
+	# λίστα με data και labels
 	data = []
 	labels = []
  
-	# loop over the rows of the A-Z handwritten digit dataset
+	# περνά σε κάθε στήλη του αρχείου
 	for row in open(datasetPath):
-		# parse the label and image from the row
+		#διαχωρίζει labels από images
 		row = row.split(",")
 		label = int(row[0])
 		image = np.array([int(x) for x in row[1:]], dtype="uint8")
   
-		# images are represented as single channel (grayscale) images
-		# that are 28x28=784 pixels -- we need to take this flattened
-		# 784-d list of numbers and repshape them into a 28x28 matrix
+		# μετατρέπει την εικόνα από λίστα με 784 στοιχεία(αναπαριστούν το grascale)
+		# σε 28*28 πίνακα
+		 
 		image = image.reshape((28, 28))
   
-		# update the list of data and labels
+		# προσθέτει images και labels στις αντίστοιχες λίστες
 		data.append(image)
 		labels.append(label)
   
-  # convert the data and labels to NumPy arrays
+  # μεττρέπει τα data kai τα labels σε NumPy arrays
 	data = np.array(data, dtype="float32")
 	labels = np.array(labels, dtype="int")
  
-	# return a 2-tuple of the A-Z data and labels
+	# επιστρέφει tuple με data και labels
 	return (data, labels)
 
 def load_mnist_dataset():
-	# load the MNIST dataset and stack the training data and testing
-	# data together (we'll create our own training and testing splits
-	# later in the project)
+	# φπρτώνει το MNIST dataset και ενώνει τα test data και train data
+	#και τα test labels με τα train labels
+	
 	((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
 	data = np.vstack([trainData, testData])
 	labels = np.hstack([trainLabels, testLabels])
  
-	# return a 2-tuple of the MNIST data and labels
+	# επιστρέφει tuplei με data και labels
 	return (data, labels)
 
